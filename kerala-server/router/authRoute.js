fe73608ebcaa536177f251221c9f7b7ff48d3ecf
mohammedkoya7356 <muhammadkoya04@gmail.com>
2025-07-19 20:54:25 +0530

    ✅ Sync all files to GitHub

--- kerala-server/router/authRoute.js
+++ kerala-server/router/authRoute.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controller/authController');
+const authController = require('../controller/authController');
 
+// @route   POST /api/auth/register
+// @desc    Register new user (admin/user)
+// @access  Public
+router.post('/register', authController.register);
 
-router.post('/register', register);
-
-
-router.post('/login', login);
+// @route   POST /api/auth/login
+// @desc    Login as admin (only)
+// @access  Public
+router.post('/login', authController.login);
 
 module.exports = router;
